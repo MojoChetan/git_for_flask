74ea20c5d3996a2d3fef7ba8331dfb8b8350376b
unknown <nikita.sonankar@db.com>
2019-10-21 11:08:57 +0530

    changing the code

--- virtual_1/hello.py
+++ virtual_1/hello.py
@@ -1,7 +1,8 @@
-from flask import Flask
-app = Flask(__name__)
+#/from flask import Flask
+#app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    z='test'
-    return z
+#@app.route('/')
+#def hello():
+#    z='test'
+#    return z 
+print("Hello Git")
